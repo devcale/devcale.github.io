63f36b37c33c6d526f124ff4db8301c104419b92
cale <ne.calero@uniandes.edu.co>
2023-01-16 20:06:57 -0500

    Working on skills circle animation

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,33 +1,72 @@
-import './Skills.css';
-import { Col, Container, Row } from "react-bootstrap";
+import "./Skills.css";
+import { Col, Container, Row, Carousel } from "react-bootstrap";
 
 export const Skills = () => {
   return (
-    <section className="skills" >
-        <div className='skills-stopper' id="skills"></div>
-        <Container className="skills-container d-flex align-items-center" >
-        <Row className="d-flex align-items-center">
-          <Col xs={12} s={10} md={7} xl={7} >
-            <h1>
-              {`My main skills are: `}
-            </h1>
-            <p >
-              Entry level Software Engineer with a keen desire to grow. Strong
-              knowledge in multiple front-end and back-end development
-              frameworks, data structures, database management, algorithm design
-              and analysis. Experience in developing software for cloud
-              architecture with the best security practices. Ready to make an
-              impact in the next team I join, and become the best in what I do.
-            </p>
-            
-          </Col>
-          <Col xs={0} s={2} md={5} xl={5} className="test-skill">
-            <p> test </p>
-            
-          </Col>
-          
-        </Row>
-      </Container>
+    <section className="skills">
+      <div className="skills-stopper" id="skills"></div>
+      <div className="container-container d-flex align-items-center">
+        <Container className="skills-container ">
+          <Row className="rowo-1  ">
+            <Col className="colo-1">
+              <h1>{`My main skills are: `}</h1>
+            </Col>
+          </Row>
+          <Row className="rowo-2">
+            <Carousel className="skills-carousel">
+              <Carousel.Item>
+                <Container className="container-skill">
+                  <Row className="row-skill">
+                    <Col className="col-skill left-skill d-flex align-items-center justify-content-center">
+                      <div className="justify-content-center ">
+                        <svg class="pie">
+                          <circle cx="100" cy="100" r="80"></circle>
+                          <path d="
+                          M 100 100 
+                          L 100 10 
+                          A 80,80 1 0,1 178,55  z
+                          z
+                          "></path>
+                        </svg>
+
+                        <p className="skill-name">JavaScript</p>
+                      </div>
+                    </Col>
+                    <Col className="col-skill center-skill d-flex align-items-center justify-content-center">
+                      <div className="justify-content-center ">
+                        <div className="circle d-flex align-items-center justify-content-center">
+                          <div className="circle-overlay">
+                            <p></p>
+                          </div>
+                        </div>
+
+                        <p className="skill-name">Go</p>
+                      </div>
+                    </Col>
+                    <Col className="col-skill right-skill d-flex align-items-center justify-content-center">
+                      <div className="justify-content-center ">
+                        <div className="circle d-flex align-items-center justify-content-center">
+                          <div className="circle-overlay">
+                            <p></p>
+                          </div>
+                        </div>
+
+                        <p className="skill-name">Python</p>
+                      </div>
+                    </Col>
+                  </Row>
+                </Container>
+              </Carousel.Item>
+              {/* <Carousel.Item>
+                <div></div>
+              </Carousel.Item>
+              <Carousel.Item>
+                <div></div>
+              </Carousel.Item> */}
+            </Carousel>
+          </Row>
+        </Container>
+      </div>
     </section>
-  )
-}
+  );
+};
